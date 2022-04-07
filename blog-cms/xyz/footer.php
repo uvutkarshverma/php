b127252640878ac7cb2915d75e58b543ceda1549
Utkarsh <utkarsh.verma9598@gmail.com>
2022-04-07 10:51:59 +0530

    added new files

--- blog-cms/xyz/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="footer max-width1 m-auto">
-        <h2>Copyright &copy Qnastop.com | All Right Reserved- Developed by Utkarsh</p>
-    </div>
-
-    </body> 
-</html> 
--- blog-cms/xyz/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-//include config PHP file 
-require_once('../include/config.php');
-
-//log user out
-$user->logout();
-header('Location: index.php'); 
-
-?> 
